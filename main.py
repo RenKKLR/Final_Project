f77c72246b5a6c1de7fc805882dc83d4b64e5a7b
Ren <ren.liu@gmx.de>
2023-11-20 16:06:18 +0100

    correct method "sell stock" add sell.csv

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class Transaction:
                        f"{transaction.price};"
                        f"{transaction.amount};"
                        f"{transaction.date}\n")
+            print(f"Stock {stock.name} in {amount} share(s) has been saved successfully!")
 
     @staticmethod
     def single_stock_count(filename, stock_name):
@@ -32,6 +33,7 @@ class Transaction:
             for transaction in transactions:
                 if stock_name == transaction[0]:
                     single_stock_count += int(transaction[3])
+
         return single_stock_count
 
     @staticmethod
@@ -50,27 +52,66 @@ class Transaction:
         stock_name = sell_transaction.stock.name
         amount_to_sell = sell_transaction.amount
 
-        remaining_stock = Transaction.single_stock_count(filename, stock_name)
+        bought_amount = Transaction.single_stock_count("buy.csv", stock_name)
+        sold_amount = Transaction.single_stock_count("sell.csv", stock_name)
+
+        remaining_stock = bought_amount - sold_amount
 
         if remaining_stock >= amount_to_sell:
-            with open(filename, "a", newline="") as file:
+            with open("sell.csv", "a", newline="") as file:
                 file.write(f"{sell_transaction.stock.name};"
                            f"{sell_transaction.stock.stock_id};"
                            f"{sell_transaction.price};"
-                           f"{-sell_transaction.amount};"
+                           f"{sell_transaction.amount};"
                            f"{sell_transaction.date}\n")
         else:
             print(f"You don't have enough stock {stock_name} to sell {amount_to_sell} share(s).")
 
 
+    @staticmethod
+    def calculate_single_stock_performance(stock_name):
+        bought_amount = 0
+        sold_amount = 0
+       #buy_transactions = []
+        #sell_transactions = []
+
+        with open("buy.csv") as buy_file:
+            buy_transactions = csv.reader(buy_file, delimiter=";")
+            for buy_transaction in buy_transactions:
+                if stock_name == buy_transaction[0]:
+                    bought_amount += int(buy_transaction[3])
+                    print("Bought: ", buy_transaction)
+
+        with open("sell.csv") as sell_file:
+            sell_transactions = csv.reader(sell_file, delimiter=";")
+            for sell_transaction in sell_transactions:
+                if stock_name == sell_transaction[0]:
+                    sold_amount += int(sell_transaction[3])
+                    print("Sold: ", sell_transaction)
+
+        if bought_amount == 0:
+            print(f"You haven't bought stock {stock_name} yet.")
+
+        else:
+            pass
+
+
+
+
+
+
+
 stock = Stock("Amazon", "12345")
 price = 55
-amount = 380
+amount = 10000
 date = "2023-11-20"
 
 transaction = Transaction(stock, price, amount, date)
 
-transaction.sell_stock("transaction.csv", transaction)
+#transaction.save_transaction("buy.csv", transaction)
+transaction.sell_stock("buy.csv", transaction)
+#transaction.calculate_single_stock_performance("An")
+
 
 '''def main():
 
