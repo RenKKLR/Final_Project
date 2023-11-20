72f2be34e0a22fb9ea5adbcd1a3d24adbfc0d3e9
Ren <ren.liu@gmx.de>
2023-11-20 17:18:14 +0100

    add calculate_single_stock_performance

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Transaction:
                        f"{transaction.price};"
                        f"{transaction.amount};"
                        f"{transaction.date}\n")
-            print(f"Stock {stock.name} in {amount} share(s) has been saved successfully!")
+            print(f"Stock {stock.name} in {amount} share(s) has been saved in {filename} successfully!")
 
     @staticmethod
     def single_stock_count(filename, stock_name):
@@ -64,6 +64,7 @@ class Transaction:
                            f"{sell_transaction.price};"
                            f"{sell_transaction.amount};"
                            f"{sell_transaction.date}\n")
+                print(f"Stock {stock_name} in {amount} share(s) has been saved in sell.csv successfully.")
         else:
             print(f"You don't have enough stock {stock_name} to sell {amount_to_sell} share(s).")
 
@@ -74,26 +75,45 @@ class Transaction:
         sold_amount = 0
        #buy_transactions = []
         #sell_transactions = []
+        def performance(sell_sum, buy_sum):
+            return (sell_sum -buy_sum) / buy_sum * 100
 
         with open("buy.csv") as buy_file:
             buy_transactions = csv.reader(buy_file, delimiter=";")
             for buy_transaction in buy_transactions:
                 if stock_name == buy_transaction[0]:
                     bought_amount += int(buy_transaction[3])
+                    bought_value = bought_amount * float(buy_transaction[2])
                     print("Bought: ", buy_transaction)
+
 
         with open("sell.csv") as sell_file:
             sell_transactions = csv.reader(sell_file, delimiter=";")
             for sell_transaction in sell_transactions:
                 if stock_name == sell_transaction[0]:
                     sold_amount += int(sell_transaction[3])
+                    sold_value = sold_amount * float(sell_transaction[2])
                     print("Sold: ", sell_transaction)
 
         if bought_amount == 0:
             print(f"You haven't bought stock {stock_name} yet.")
 
+        elif bought_amount > sold_amount:
+            current_price = float(input("Please enter the current selling price: "))
+            remaining_shares = bought_amount - sold_amount
+            current_value = current_price * remaining_shares
+            single_stock_performance = performance(sold_value+current_value, bought_value)
+            print(f"Current value of stock {stock_name}: €{current_value:.2f}")
+            print(f"Bought value of stock {stock_name}: €{bought_value:.2f}")
+            print(f"Sold value of stock {stock_name}: €{sold_value:.2f}")
+            print(f"Performance of stock {stock_name}: {single_stock_performance:+.2f}%")
+
         else:
-            pass
+            single_stock_performance = performance(sold_value, bought_value)
+            print(f"Bought value of stock {stock_name}: €{bought_value:.2f}")
+            print(f"Sold value of stock {stock_name}: €{sold_value:.2f}")
+            print(f"Performance of stock {stock_name}: {single_stock_performance:+.2f}%")
+
 
 
 
@@ -102,15 +122,15 @@ class Transaction:
 
 
 stock = Stock("Amazon", "12345")
-price = 55
-amount = 10000
+price = 75
+amount = 290
 date = "2023-11-20"
 
 transaction = Transaction(stock, price, amount, date)
 
 #transaction.save_transaction("buy.csv", transaction)
 transaction.sell_stock("buy.csv", transaction)
-#transaction.calculate_single_stock_performance("An")
+#transaction.calculate_single_stock_performance("Amazon")
 
 
 '''def main():
